8e1768add7f8ad54b8f804e6de5e4bb3d749f749
João Vittor de Moura Padilha <103512187+JVittorMP@users.noreply.github.com>
2022-06-23 16:45:11 -0400

    Atualização

---
+++ Avaliação 2/Funcoes.h
@@ -0,0 +1,12 @@
+#ifndef AV2_FUNCAO4_H
+#define AV2_FUNCAO4_H
+
+void NovaVenda();
+bool ProcuraCliente();
+int DefinirIdentificacaoDaVenda();
+void AdicionarItemCompra();
+void MostrarProduto();
+void ReduzirEstoque();
+void IncluirProdutos();
+
+#endif //AV2_FUNCAO4_H
---
+++ Avaliação 2/Funções1&2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Funcoes.h"
+#include "Registros.h"
+
+void IncluirProdutos(){
+    FILE *arq1
+    arq1 = fopen
+}
