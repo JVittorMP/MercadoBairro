da2d9462bc4df932c2b0def0bbfeef69937a28ce
João Vittor de Moura Padilha <103512187+JVittorMP@users.noreply.github.com>
2022-06-29 18:12:12 -0400

    Função Fim de Arquivo - Alteração

--- Avaliação 2/Funções1&2.c
+++ Avaliação 2/Funções1&2.c
@@ -40,7 +40,7 @@ int Identificar_Produto(){
     {
         printf("Qual produto deseja alterar? (Insira id): ");
         scanf(" %d", &id_prod);
-        while(arqv != feof(arqv))
+        while(!feof(arqv))
         {
             fread(Ident, sizeof(Produto), 1, arqv);
             if(Ident.id == id_prod)
