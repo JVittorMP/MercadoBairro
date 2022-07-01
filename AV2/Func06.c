467c26dab34411780de166a29ec7959c47060656
AlineANakazawa <107937439+AlineANakazawa@users.noreply.github.com>
2022-07-01 10:34:13 -0400

    Update Func06.c
    Agora ele faz a mudança por parâmetro de referência

--- AV2/Func06.c
+++ AV2/Func06.c
@@ -8,28 +8,28 @@ void ListarVendas () {
     FILE *venda;
     Vendas compra;
     int opcao;
-    char cpf[13], nome[30];
+    char nome[30], cpf[13] = NULL;
 
     printf ("Deseja acessar por:\n1.CPF\n2.Nome");
     scanf ("%d", &opcao);
     switch (opcao)
     {
-        case 1:
-            printf ("Digite CPF: ");
-            scanf ("%[^\n]s", cpf);
-            break;
-        case 2:
-            printf ("Digite nome: ");
-            scanf (" %[^\n]s", nome);
-            strcpy (cpf, CPFDoCliente(nome)); //Arrumar cópia para devolver a string
-            break;
-        default:
-            printf ("Opção inválida!");
-            break;
+    case 1:
+        printf ("Digite CPF: ");
+        scanf ("%[^\n]s", cpf);
+        break;
+    case 2:
+        printf ("Digite nome: ");
+        scanf (" %[^\n]s", nome);
+        CPFdoCliente(nome, &cpf);
+        break;
+    default:
+        printf ("Opção inválida!\n\n");
+        break;
     }
     //SEPARAR TELA
-    if (strlen(cpf) < 13)
-        printf ("Não foi possívle acessar lista.");
+    if (cpf == NULL)
+        printf ("Não foi possívle acessar lista.\n\n");
     else {
         venda = fopen ("Vendas.dat", "rb");
         if (venda == NULL)
@@ -39,7 +39,7 @@ void ListarVendas () {
                 if (strcmp(compra.CPF, cpf) == 0) {
                     printf ("Data: %d / %d / %d\n", compra.compra.dia, compra.compra.mes, compra.compra.ano);
                     printf ("Valor: %f\n", compra.valor_total);
-                    printf ("Quantidade de produtos: %d\n", compra.quant_prod);
+                    printf ("Quantidade de produtos: %d\n\n", compra.quant_prod);
                     //SEPARAR TELA
                 }
             }
@@ -48,14 +48,14 @@ void ListarVendas () {
     }
 }
 
-char CPFDoCliente (char nome[30]) {
+void CPFdoCliente (char nome[30], char *cpf[13]) {
     FILE *cliente;
-    Clientes info;
+    Clientes info = NULL;
     bool encontrado = false;
 
     cliente = fopen ("Clientes.dat", "rb");
     if (cliente == NULL)
-        printf ("Erro na abertura do arquuivo de clientes");
+        printf ("Erro na abertura do arquuivo de clientes\n\n");
     else {
         while ((!encontrado) && (fread(&info, sizeof(Clientes), 1, cliente) != 0)) {
             if (strcmp(info.nome, nome) == 0)
@@ -63,5 +63,5 @@ char CPFDoCliente (char nome[30]) {
         }
     }
     fclose(cliente);
-    return (info.CPF);
+    strcpy(*cpf, info.CPF);
 }
