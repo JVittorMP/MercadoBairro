7b9b3398a3b326b74b06767f099435d585891721
João Vittor de Moura Padilha <joao_vittor-2003@hotmail.com>
2022-07-02 13:55:03 -0400

    Atualização Interface

--- src/Menus.c
+++ src/Menus.c
@@ -6,8 +6,7 @@
 
 void Menu_Principal(){
     int menu;
-
-    // Menu Inicial
+    printf("Menu Principal \n\n");
     printf(" 1. Vendas \n 2. Clientes \n 3. Produtos \n 9. Sair \n\n");
     printf("Insira a opção desejada: ");
     scanf(" %d", &menu);
@@ -31,6 +30,7 @@ void Menu_Principal(){
 
 void Menu_Vendas(){
     int menu;
+    printf("Menu Vendas \n\n");
     printf(" 1. Nova Venda \n 2. Listar Venda do Cliente \n 9. Sair \n\n");
     printf("Insira a opção desejada: ");
     scanf(" %d", &menu);
@@ -48,6 +48,7 @@ void Menu_Vendas(){
 
 void Menu_Clientes(){
     int menu;
+    printf("Menu Clientes \n\n");
     printf(" 1. Cadastrar Novo Cliente \n 2. Atualizar Pontuação \n");
     printf(" 3. Atualizar Cliente \n 4. Listar Clientes entre 18 e 25 anos \n");
     printf(" 5. Listar Clientes Acima de 1000 Pontos \n 9. Sair \n\n");
@@ -73,6 +74,7 @@ void Menu_Clientes(){
 
 void Menu_Produto(){
     int menu;
+    printf("Menu Produtos \n\n");
     printf(" 1. Cadastrar Novo produto \n 2. Atualizar Informações \n");
     printf(" 3. Estoque por Setor \n 4. Produtos com Estoque Baixo \n 9. Sair \n\n");
     printf("Insira a opção desejada: ");
