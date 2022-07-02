7a56a32ef05c3b13dff88cad21597ff315a92424
João Vittor de Moura Padilha <joao_vittor-2003@hotmail.com>
2022-07-02 13:31:24 -0400

    Teste do Arquivo

--- src/Principal.c
+++ src/Principal.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <windows.h>
+#include <stdbool.h>
 #include "Registros.h"
 #include "Menus.h"
 
 int main(){
     SetConsoleOutputCP(65001);
     Menu_Principal();
+
+    // Teste de Impressão (Pode ser utilizado para imprimir os registros em outras funções)
+    /*
+    Produto *pol;
+    pol = (Produto *) malloc(sizeof(Produto) * 2);
+    char confirm;
+    bool sair = false;
+    FILE *arqv;
+    arqv = fopen("../Produtos.dat","rb");
+    if(arqv != NULL){
+        fread(pol, sizeof(Produto), 2, arqv);
+        for(int c = 0; c < 2; c++){
+            printf(" ID: %d \n Setor: %s \n Nome: %s \n Preço: %.2lf \n", pol[c].id, pol[c].setor, pol[c].nome, pol[c].preco);
+            printf(" Validade: %d/%d/%d \n Estoque: %d \n\n", pol[c].validade.dia, pol[c].validade.mes, pol[c].validade.ano, pol[c].estoque);
+        }
+        while(!sair){
+            printf("Deseja encerrar o programa? (S/N) ");
+            scanf(" %c", &confirm);
+            if(confirm == 's' || confirm == 'S')
+                sair = true;
+            else
+                sair = false;
+        }
+        fclose(arqv);
+    }
+    */
 }
